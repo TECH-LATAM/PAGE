c2733958a9e778b8edead9d25cc1b551d7513755
Cristian_Casa <85920489+CristianCasa@users.noreply.github.com>
2024-12-03 18:00:36 -0500

    act. prod. secciones

--- ESTILO/js/main.js
+++ ESTILO/js/main.js
@@ -1,33 +1,27 @@
-const productosproximos = [
+const productos2 = [
     { 
         nombre: "LOVELEDI - 15000 mAh", 
-        precio: 29.99, 
+        precio: 30, 
         img: "https://m.media-amazon.com/images/I/61EUFa99OtL._AC_UF1000,1000_QL80_FMwebp_.jpg", 
         descripcion: "Batería externa de alta capacidad, ideal para cargar varios dispositivos a la vez."
     },
     { 
         nombre: "JIGA - 30000 mAh", 
-        precio: 34.99, 
+        precio: 35, 
         img: "https://m.media-amazon.com/images/I/61YbQo+JqJL._AC_SX569_.jpg", 
         descripcion: "Cargador portátil con capacidad de 30000 mAh, compatible con smartphones y tablets."
     },
     { 
         nombre: "TOZO - 20000 mAh", 
-        precio: 29.99, 
+        precio: 30, 
         img: "https://m.media-amazon.com/images/I/61VU+ZfA3kL._AC_SX679_.jpg", 
         descripcion: "Powerbank ultradelgado con carga rápida, diseño compacto y liviano de 20000 mAh. "
     },
     { 
         nombre: "INIU - 20000 mAh",  
-        precio: 49.99, 
+        precio: 50, 
         img: "https://m.media-amazon.com/images/I/51aZGPrKBbL._AC_SX679_.jpg", 
         descripcion: "Cargador portátil premium, diseño elegante y alta capacidad para dispositivos de alta carga."
-    },
-    {
-        nombre: "Ruedas omnidireccionales",  
-        precio: 12, 
-        img: "https://avelectronics.cc/wp-content/uploads/2021/12/ma006-1.jpg", 
-        descripcion:"Par De Ruedas Llantas Omnidireccionales 60mm"
     },
 ];
 
@@ -39,19 +33,19 @@ const productos = [
         descripcion:"Par De Ruedas Llantas Omnidireccionales 60mm"
     },
     {
-        nombre: "ESP32-CAM",  
+        nombre: " ESP32-CAM",  
         precio: 12, 
         img: "https://http2.mlstatic.com/D_NQ_NP_2X_894956-MEC54959362481_042023-F.webp", 
-        descripcion:"ESP32-CAM ESP32-CAM-MB MICRO USB ESP32 Serial to WiFi ESP32 CAM Development Board CH340 CH340G 5V Bluetooth+OV2640 Camera"
+        descripcion:"ESP32-CAM MICRO USB ESP32 Serial Development Board CH340"
     },
     {
         nombre: "SG90 Servo motor micro 9g 360",  
         precio: 2, 
         img: "https://ae-pic-a1.aliexpress-media.com/kf/Seaafa79b597b4c668412b98af6a46b36l/2-5-10-20pcs-SG90-Servo-motor-micro-9g-180-360-degree-mount-SG90-kit-without.jpg_.webp", 
-        descripcion:"SG90 Servo motor micro 9g 360 degree mount SG90 kit without metal gear pro RC Car Toy Airplane Arduino UNO DIY"
+        descripcion:"SG90 Servo motor micro 9g 360 degree mount SG90 "
     },
     {
-        nombre: "Moto reductor DC",  
+        nombre: "360",  
         precio: 2, 
         img: "https://http2.mlstatic.com/D_NQ_NP_2X_734121-MEC74251876976_022024-F.webp", 
         descripcion:"Mgsystem Motor Reductor Motorreductor Biaxial Arduino Robot"
@@ -90,19 +84,21 @@ function cargarProductos() {
     });
 }
 
-function cargarProductosProximos() {
-    const contenedorProductosProximos = document.getElementById("productosProximos");
-    productosproximos.forEach(producto => {
+function cargarProductos2() {
+    const contenedorProductos2 = document.getElementById("productos2");
+    productos2.forEach(producto2 => {
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
-            <img src="${productosproximos.img}" alt="${productosproximos.nombre}">
-            <h5>${productosproximos.nombre}</h5>
-            <p>Precio: $${productosproximos.precio.toFixed(2)}</p>
-            <p><strong>Descripción:</strong> ${productosproximos.descripcion}</p>
+            <img src="${producto2.img}" alt="${producto2.nombre}">
+            <h5>${producto2.nombre}</h5>
+            <p>Precio: $${producto2.precio.toFixed(2)}</p>
+            <p><strong>Descripción:</strong> ${producto2.descripcion}</p>
+            <button class="agregar-carrito" data-nombre="${producto2.nombre}" data-precio="${producto2.precio}">Proximamente</button>
         `;
-        contenedorProductosProximos.appendChild(div);
+        contenedorProductos2.appendChild(div);
     });
+
 }
 
 document.getElementById("carrito-icono").addEventListener("click", () => {
@@ -173,3 +169,4 @@ function enviarWhatsApp() {
 }
 
 cargarProductos();
+cargarProductos2();
